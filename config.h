f08a92d47f683159f8c3e4c7ec9e2fbe5e44630b
srttk <saroteki26@gmail.com>
2020-01-10 18:31:26 +0900

    initialize

---
+++ config.h
@@ -0,0 +1,11 @@
+#include "../../config.h"
+
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
